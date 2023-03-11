ccfe09a248381218cdc17b86fbd0b4d6853a62a9
AlexandraMurgoci <alexa.murgoci@gmail.com>
2023-03-11 21:15:23 +0200

    lab 3 - clasa Complex completa, separata in .h si .cpp, poate fi folosita pentru proiecte

---
+++ complex/ComplexTest.h
@@ -0,0 +1,24 @@
+
+#ifndef POO_LAB_144_2022_2023_SEMIGRUPA_2_COMPLEXTEST_H
+#define POO_LAB_144_2022_2023_SEMIGRUPA_2_COMPLEXTEST_H
+
+#include "Complex.h"
+
+class ComplexTest {
+    void TestGetterAndSetter();
+    void TestConstructorInit();
+    void FunctieExempluParametruPrinValoare(Complex);
+    void FunctieExempluParametruPrinReferinta(Complex&);
+    void TestConstructorCopiere();
+    void TestOperatorPlus();
+    void TestOperatorInmultire();
+    void TestOperatorImpartire();
+    void TestOperatorEgal();
+    void TestOperatorCitire();
+    void TestOperatorAfisare();
+    void TestAbsSquare();
+public:
+    void Test();
+};
+
+#endif //POO_LAB_144_2022_2023_SEMIGRUPA_2_COMPLEXTEST_H
